05f300f0a59021b1010eb55eb794c782bf86713f
mdbicode <mdbahrulilmi@gmail.com>
2026-08-19 14:49:22 +0700

    feat: add broadcast

--- server.go
+++ server.go
@@ -5,94 +5,23 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"os"
-	"regexp"
 
 	"github.com/mdbahrulilmi/realtime-chat-server/ws"
 )
 
-type Page struct {
-    Title string
-    Body  []byte
-}
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html","ws.html"))
-
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return  os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error)  {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return  nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    err := templates.ExecuteTemplate(w, tmpl+".html", p)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-
-var validPath = regexp.MustCompile("^/(edit|save|view|ws-test)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        m := validPath.FindStringSubmatch(r.URL.Path)
-        if m == nil {
-            http.NotFound(w, r)
-            return
-        }
-        fn(w, r, m[2])
-    }
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit", p)
-}
-
-func wsHandler(w http.ResponseWriter, r *http.Request) {
-
-    renderTemplate(w, "ws", nil)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-    body := r.FormValue("body")
-    p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
+func Handler(w http.ResponseWriter, r *http.Request) {
+    tmpl, err := template.ParseFiles("index.html")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+    tmpl.Execute(w, nil)
 }
 
 func main() {
 	port := ":2500"
 	http.HandleFunc("/ws", ws.Handler)
-	http.HandleFunc("/ws-test", wsHandler)
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-    http.HandleFunc("/edit/", makeHandler(editHandler))
-    http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", Handler)
 	fmt.Printf("server runnning at http://localhost%s ", port)
     log.Fatal(http.ListenAndServe(port, nil))
 }
--- ws/ws.go
+++ ws/ws.go
@@ -8,37 +8,62 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  1024,
-    WriteBufferSize: 1024,
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+var connections []*websocket.Conn
+
+func removeConnection(conn *websocket.Conn) {
+	for i, client := range connections {
+		if client == conn {
+			connections = append(
+				connections[:i],
+				connections[i+1:]...,
+			)
+
+			break
+		}
+	}
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    log.Println("1. HTTP request masuk")
+	log.Println("HTTP request masuk")
 
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Println("2. Upgrade gagal:", err)
-        return
-    }
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade gagal:", err)
+		return
+	}
 
-    log.Println("2. HTTP berhasil di-upgrade menjadi WebSocket")
+	defer conn.Close()
 
-    defer conn.Close()
+	connections = append(connections, conn)
 
-    for {
-        messageType, p, err := conn.ReadMessage()
-        if err != nil {
-            log.Println("3. Read gagal:", err)
-            return
+	log.Println("Jumlah client:", len(connections))
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read gagal:", err)
+            removeConnection(conn)
+            log.Println("Jumlah client:", len(connections))
+			return
+		}
+
+		for _, client := range connections {
+            if client == conn {
+                continue
+            }
+
+            err := client.WriteMessage(
+                websocket.TextMessage,
+                message,
+            )
+
+            if err != nil {
+                log.Println("Broadcast error:", err)
+            }
         }
-
-        log.Printf("3. Message diterima: %s", p)
-
-        if err := conn.WriteMessage(messageType, p); err != nil {
-            log.Printf("4. Message gagal dikirim: %v", err)
-            return
-        }
-
-        log.Printf("4. Message dikirim kembali: %s", p)
-    }
+	}
 }
